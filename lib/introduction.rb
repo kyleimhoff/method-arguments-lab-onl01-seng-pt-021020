7904a288de1081ec15b1910d6fc8933f242f386c
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-11 03:47:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,6 @@
 def introduction_with_language
   puts "Hello, my name is #{name}."
 end
-
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
